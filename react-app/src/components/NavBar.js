fbac8fd8f74e031bdb4a38ab43e5316efdd8c6ab
nunezjavier <javin7298@gmail.com>
2022-07-28 15:31:15 -0300

    NavBar

---
+++ react-app/src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <nav className="navbar barraNav">
+            <h1 className="titulo">JVITECH</h1>
+            <ul>
+                <li>Productos</li>
+                <li>Mis compras</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
